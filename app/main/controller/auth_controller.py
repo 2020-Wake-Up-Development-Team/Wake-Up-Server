028a28f4c6832b095f3f2cfa24062479bac09429
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-11-24 15:57:13 +0900

    INIT auth branch

--- app/main/controller/auth_controller.py
+++ app/main/controller/auth_controller.py
@@ -1,3 +1,39 @@
-from flask import Blueprint, request, json, Response
+from flask import Flask, request, Blueprint, jsonify
+from ..service import auth_service
+
 
 api = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@api.route("/signup", methods=["POST"])  # 회원가입
+def signup():
+    data = request.get_json()  # 데이터 받기
+
+    state = auth_service.user_signup(
+        data["user_id"], data["password"], data["school"], data["number"], data["name"]
+    )  # 회원가입 함수
+
+    if state == "success":
+        return jsonify({"code": 200, "message": "signup success"})  # 회원가입 성공
+
+    elif state == "defind id":
+        return jsonify({"code": 400, "message": "signup defind id"})  # 이미 있는 아이디
+
+    elif state == "fail":
+        return jsonify({"code": 400, "message": "signup fail"})  # 회원가입 실패
+
+
+@api.route("/login", methods=["POST"])  # 로그인
+def login():
+    data = request.get_json()  # 데이터 받기
+
+    state = auth_service.user_login(data["user_id"], data["password"])  # 로그인 함수
+
+    if state == "success":
+        return jsonify({"code": 200, "message": "login success"})  # 로그인 성공
+
+    elif state == "fail":
+        return jsonify({"code": 400, "message": "login fail"})  # 로그인 실패
+
+    elif state == "undefind id":
+        return jsonify({"code": 400, "message": "login undefined id"})  # 없는 아이디
--- app/main/service/auth_service.py
+++ app/main/service/auth_service.py
@@ -1,1 +1,55 @@
 # USERS 테이블에 접근하는 파일
+from ..model.models import USERS_TB
+from ...db import db_session
+
+
+def user_signup(user_id, pwd, school, number, name):
+    try:
+        if find_id(user_id):  # 이미 있는 아이디
+            print("defind")
+            return "defind id"
+
+        else:  # 회원가입 진행
+            table = USERS_TB(user_id=user_id, pwd=pwd, school=school, name=name)
+            db_session.add(table)
+            db_session.commit()
+            return "success"  # 회원가입 성공
+
+    except Exception as err:
+        print("Error Log: [{}]".format(err))
+        return "fail"
+
+
+def user_login(user_id, pwd):
+    try:
+        if not find_id(user_id):  # 없는 아이디로 로그인
+            return "undefind id"
+        else:
+            queries = (
+                db_session.query(USERS_TB)
+                .filter(USERS_TB.user_id == user_id)
+                .filter(USERS_TB.pwd == pwd)
+            )
+            entry = [dict(user_id=q.user_id, pwd=q.pwd) for q in queries]
+            if len(entry) == 0:  # 로그인 실패
+                return "fail"
+            else:
+                return "success"  # 로그인 성공
+
+    except Exception as err:
+        print("Error Log: [{}]".format(err))
+        return "fail"
+
+
+def find_id(user_id):  # 아이디가 있으면 return 1, 없으면 return 0
+    try:
+        queries = db_session.query(USERS_TB).filter(USERS_TB.user_id == user_id)
+        entry = [dict(user_id=q.user_id, pwd=q.pwd) for q in queries]
+        if len(entry) == 0:
+            return False  # 아이디 X
+        else:
+            return True  # 아이디 O
+
+    except Exception as err:
+        print("Error Log: [{}]".format(err))
+        return 0
